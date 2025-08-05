40c285890c8ab479850deb162e6e2d368d66a304
EnriqueIsra <sirilo99x3@gmail.com>
2025-08-05 10:46:40 -0600

    137. Herencia

--- introduccion_JS/js/28-POO-ClasesEnJavaScript.js
+++ introduccion_JS/js/28-POO-ClasesEnJavaScript.js
@@ -7,30 +7,45 @@ class Producto {
     this.disponible = disponible;
   }
   formatearProducto() {
-    return `El producto ${this.nombre} tiene un precio de $${this.precio} y esta ${this.disponible}`;
+    return `El Producto ${this.nombre} tiene un precio de $${this.precio} y esta ${this.disponible}`;
   }
 }
 
 const producto2 = new Producto(
   'Monitor Curvo de 40"',
   800,
-  false ? "disponible" : "NO disponible"
+  false ? "disponible" : "agotado"
 );
 const producto3 = new Producto(
   "Laptop",
   300,
-  true ? "disponible" : "NO disponible"
+  true ? "disponible" : "agotado"
 );
 const producto4 = new Producto(
   "Televisión de 50 pulgadas",
   2000,
-  false ? "disponible" : "NO disponible"
+  false ? "disponible" : "agotado"
 );
 const producto5 = new Producto(
   "Audifonos de DJ",
   600,
-  true ? "disponible" : "NO disponible"
+  true ? "disponible" : "agotado"
 );
+
+//clase hija
+class Libro extends Producto {
+    constructor(nombre, precio, disponible, isbn) {
+        super(nombre, precio, disponible)
+        this.isbn = isbn
+    }
+    formatearLibro() {
+    return `${super.formatearProducto()}, isbn: ${this.isbn}`;
+  }
+}
+
+const libro = new Libro('Harry Potter', 900, false ? "disponible" : "agotado", '897613857498165')
 
 console.log(producto2);
 console.log(producto3.formatearProducto());
+console.log(libro)
+console.log(libro.formatearLibro())
