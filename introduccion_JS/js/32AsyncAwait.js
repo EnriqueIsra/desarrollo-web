3c043bad05a8eb66a790a14e9314ecbd431440cd
EnriqueIsra <sirilo99x3@gmail.com>
2025-08-05 19:12:03 -0600

    142. ¿Cómo trabajar con dos consultas async await? y 143. Fetch API

--- introduccion_JS/js/32AsyncAwait.js
+++ introduccion_JS/js/32AsyncAwait.js
@@ -9,11 +9,21 @@ function descargarNuevosClientes() {
         }, 5000 )
     })
 }
+function descargarUltimosPedidos() {
+    return new Promise( resolve => {
+        console.log('Descargando pedidos, espere...')
+
+        setTimeout( () => {
+            resolve('Los pedidos han sido descargados')
+        }, 5000 )
+    })
+}
 
 async function app() {
     try {
-        const resultado = await descargarNuevosClientes()
-        console.log(resultado)
+        const resultado = await Promise.all([descargarNuevosClientes(), descargarUltimosPedidos()])
+        console.log(resultado[0])
+        console.log(resultado[1])
     } catch (error) {
         console.error(error)
     }
