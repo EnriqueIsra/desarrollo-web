b0d8df98bbba01ef1676ce1d027780b9246c0cf8
EnriqueIsra <sirilo99x3@gmail.com>
2025-08-05 18:15:17 -0600

    141 async / await

---
+++ introduccion_JS/js/32AsyncAwait.js
@@ -0,0 +1,21 @@
+// async // await
+
+function descargarNuevosClientes() {
+    return new Promise( resolve => {
+        console.log('Descargando clientes, espere...')
+
+        setTimeout( () => {
+            resolve('Los clientes han sido descargados')
+        }, 5000 )
+    })
+}
+
+async function app() {
+    try {
+        const resultado = await descargarNuevosClientes()
+        console.log(resultado)
+    } catch (error) {
+        console.error(error)
+    }
+}
+app()
