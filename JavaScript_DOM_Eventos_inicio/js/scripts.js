e9658fb599bbc0468172d3d821335e3c88759bd3
EnriqueIsra <sirilo99x3@gmail.com>
2025-09-17 19:46:49 -0600

    147.Eventos en JavaScript

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -1,31 +1,50 @@
 // querySelector -> deevuelve 0 o 1 elemento
-const heading = document.querySelector(".header__texto h2")
-console.log(heading)
+const heading = document.querySelector(".header__texto h2");
+console.log(heading);
 
 //querySelectorAll -> devuelve 0 o todos los elementos que coincidan
-const enlaces = document.querySelectorAll(".navegacion a")
-console.log(enlaces)
-console.log(enlaces[0])
-enlaces[0].textContent = "Nuevo texto para enlace"
-enlaces[0].classList.add("nueva-clase") 
-//enlaces[0].classList.remove("navegacion__enlace") 
+const enlaces = document.querySelectorAll(".navegacion a");
+console.log(enlaces);
+console.log(enlaces[0]);
+enlaces[0].textContent = "Nuevo texto para enlace";
+enlaces[0].classList.add("nueva-clase");
+//enlaces[0].classList.remove("navegacion__enlace")
 
 //getElementById
-const heading2 = document.getElementById("heading")
-console.log(heading2)
+const heading2 = document.getElementById("heading");
+console.log(heading2);
 
 // Genera enlace
 
-const nuevoEnlace = document.createElement('A')
+const nuevoEnlace = document.createElement("A");
 //agregar href
-nuevoEnlace.href = 'nuevo-enlace.html'
+nuevoEnlace.href = "nuevo-enlace.html";
 //agregar texto
-nuevoEnlace.textContent = 'Tienda Virtual'
+nuevoEnlace.textContent = "Tienda Virtual";
 //agregar clase
-nuevoEnlace.classList.add('navegacion__enlace')
+nuevoEnlace.classList.add("navegacion__enlace");
 // agregar al documento
-const navegacion = document.querySelector('.navegacion')
-navegacion.appendChild(nuevoEnlace)
+const navegacion = document.querySelector(".navegacion");
+navegacion.appendChild(nuevoEnlace);
 
-console.log(nuevoEnlace)
+console.log(nuevoEnlace);
 
+//Eventos
+
+console.log(1);
+
+window.addEventListener("load", function () {
+  console.log(2);
+});
+window.onload = function () {
+  console.log(3);
+};
+document.addEventListener('DOMContentLoaded', function () {
+    console.log(4)
+})
+
+console.log(5)
+
+window.onscroll = function(){
+    console.log('scrolling...')
+}
