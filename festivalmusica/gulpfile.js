92a0d3b8ae98f29a04c8b6f430d9ac73ad937b3a
EnriqueIsra <sirilo99x3@gmail.com>
2025-10-11 21:32:05 -0600

    166. Habilitar modo watch con Gulp

--- festivalmusica/gulpfile.js
+++ festivalmusica/gulpfile.js
@@ -1,5 +1,12 @@
-export function hola( done ) {
-    console.log('Hola mundo desde Gulp');
+import { src, dest } from "gulp";
+import gulpSass from "gulp-sass";
+import * as dartSass from "sass";
 
+const sass = gulpSass(dartSass);
+
+export function css(done) {
+    src("src/scss/app.scss")
+        .pipe(sass())
+        .pipe(dest("build/css"));
     done();
-}   
+}
