0ffe31b00d44f2e8ab190774c6e40492888f12f6
unknown <sirilo99x3@gmail.com>
2025-10-25 03:28:41 -0600

    193. Generando la Galería con JavaScript. & 194. Aplicando SASS a la galería

--- festivalmusica/src/js/app.js
+++ festivalmusica/src/js/app.js
@@ -1,1 +1,16 @@
-console.log('hola desde app.js')
+document.addEventListener("DOMContentLoaded", function () {
+    crearGaleria()
+})
+
+function crearGaleria() {
+    const galeria = document.querySelector(".galeria-imagenes")
+    const CANT_IMAGENES = 16
+
+    for (let i = 1; i <= CANT_IMAGENES; i++) {
+        const imagen = document.createElement("IMG")
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = "Imagen Galeria"
+
+        galeria.appendChild(imagen)
+    }
+}
