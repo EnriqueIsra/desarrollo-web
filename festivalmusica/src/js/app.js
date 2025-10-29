799800d91d6555b40422877bbbb4a74782d91dff
unknown <sirilo99x3@gmail.com>
2025-10-28 19:42:45 -0600

    200. Agregando un efecto de Scroll a una sección

--- festivalmusica/src/js/app.js
+++ festivalmusica/src/js/app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     navegacionFija()
     crearGaleria()
     resaltarEnlace()
+    scrollNav()
 })
 function navegacionFija() {
     const header = document.querySelector('.header')
@@ -83,6 +84,17 @@ function resaltarEnlace() {
             if(link.getAttribute('href') === '#' + actual) {
                 link.classList.add('active')
             }
+        })
+    })
+}
+function scrollNav(){
+    const navLink = document.querySelectorAll('.navegacion-principal a')
+    navLink.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault()
+            const sectionScroll = e.target.getAttribute('href')
+            const section = document.querySelector(sectionScroll)
+            section.scrollIntoView({behavior: 'smooth'})
         })
     })
 }
